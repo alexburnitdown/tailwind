abed9783c641178eb697e0721196f9a3c9eb4f82
Алексей <alexburnitdown@gmail.com>
2020-09-09 11:40:44 +0300

    tailwind кнопка Наверх

--- code.js
+++ code.js
@@ -1,5 +1,6 @@
 const burger = document.querySelector('#burger');
 const menu = document.querySelector('#menu');
+let upButton = document.querySelector('.up-button');
 
 burger.addEventListener('click', () => {
     if (menu.classList.contains('hidden')) {
@@ -8,3 +9,19 @@ burger.addEventListener('click', () => {
         menu.classList.add('hidden');
     }
 });
+
+window.onscroll = function () { 
+    console.log(window.pageYOffset);
+    if (window.pageYOffset > 70) {
+        upButton.classList.remove('hidden');
+      } else {
+          if (!upButton.classList.contains('hidden')) {
+            upButton.classList.add('hidden');
+          }
+      }
+};
+
+upButton.addEventListener('click', () => {
+    window.scrollTo(0, 0);
+});
+    
